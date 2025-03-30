7e222c24495e1242deb661c96d91ef5ad466f58f
shubam <chshivam148@gmail.com>
2025-03-31 03:07:44 +0530

    first commit

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,94 +1,66 @@
-import React ,{useRef}from 'react'
-import "./Header.css"
-import {profile7} from "../../images"
-import gsap from 'gsap'
-import { useGSAP } from '@gsap/react'
+import React from 'react';
+import "./Header.css";
+import { profile8 } from "../../images";
+import Typewriter from "typewriter-effect";
 
 const Header = () => {
-const container=useRef(null)
-gsap.registerPlugin(useGSAP)
-
-useGSAP(()=>{
-gsap.fromTo(".profile__photo__container",{
-  scale:.5,
-  duration:1,
-  opacity:0.5,
-},
-
-{
-  scale:1,
-  duration:1,
-  ease:"sine.in",
-  opacity:1,
-})
-
-// Animation for intro text
-gsap.from(".intro__text", {  
-  fontSize: 100,  
-  duration: 1,  
-  delay: 1,  
-  ease: "sine.in"  
-});
-
-// Create timeline for sequenced animations
-const timeline = gsap.timeline();
-
-timeline
-  .from(".header__info__top", {  
-      opacity: 0,
-      
-  })  
-  .from(".header__title", {  
-      opacity: 0,  
-      y: -30,
-   
-  })
-
-  .from(".header__description", {  
-    opacity: 0, 
-    
-  
-   
-})
-
-
-.from(".btn", {  
-  x:1,  
-  opacity:0,
-  stagger:.5
- 
-});
-
-},{scope:container})
-
   return (
     <header id='header' className='blur-effect'>
       <div className="stroke__text intro__text">HELLO</div>
+
       <div className="section__wrapper header__container">
+        {/* Left Side: Static Text Content */}
         <div className="column intro__container blur-effect">
           <div className="header__info">
             <div className="header__info__top">
-              Hello There! I'm <span className="color__primary">Shubam Choudhary</span>
+              Hello There! I'm 
+              <span className="color__primary">
+                {/* Typewriter Effect Only on Name */}
+                <Typewriter
+                  options={{
+                    strings: ["Shubam Choudhary"],
+                    autoStart: true,
+                    loop: true,
+                    delay: 100,
+                  }}
+                />
+              </span>
             </div>
+
             <div className="header__info__middle">
-              <h1 className="primary__title header__title">I'M A WEB DEVELOPER</h1>
+              <h1 className="primary__title header__title">
+                {/* Typewriter Effect Only on "I'm a Web Developer" */}
+                <Typewriter
+                  options={{
+                    strings: ["I'M A WEB DEVELOPER", "I AM A PROGRAMMER"],
+                    autoStart: true,
+                    loop: true,
+                    delay: 100,
+                  }}
+                />
+              </h1>
               <p className="text__muted header__description">
-              "I transform complex user needs into seamless digital experiences that combine innovation, functionality, and elegance."              </p>
+                "I transform complex user needs into seamless digital experiences that combine innovation, functionality, and elegance."
+              </p>
             </div>
+
+            {/* Static Buttons */}
             <div className="header__info__bottom nav__buttons__group">
               <button className="btn btn__primary">Download CV</button>
               <a href="mailto:chshivam148@gmail.com" className="btn btn__primary">Email Me</a>
             </div>
           </div>
         </div>
+
+        {/* Right Side: Static Profile Picture */}
         <div className="column profile__wrapper">
           <div className="profile__photo__container">
-            <img src={profile7} className="profile__photo" alt="" />
+            <img src={profile8} className="profile__photo" alt="" />
           </div>
         </div>
       </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
